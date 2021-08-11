641f74ec5300622f51c5b10d0919a3d28b7b74e7
conectado <gabrielalejandro7@gmail.com>
2021-08-11 16:40:54 -0300

    Update polkadot dependency

--- parachain/docker/build.sh
+++ parachain/docker/build.sh
@@ -5,10 +5,10 @@ pushd .
 
 # The following line ensure we run from the project root
 PROJECT_ROOT=`git rev-parse --show-toplevel`
-cd $PROJECT_ROOT/parachain
+cd $PROJECT_ROOT
 
 # Build the image
-time docker build -f ./docker/Dockerfile --build-arg RUSTC_WRAPPER= --build-arg PROFILE=release -t efinity/artemis:latest .
+time docker build -f ./parachain/docker/Dockerfile --build-arg RUSTC_WRAPPER= --build-arg PROFILE=release -t efinity/artemis:latest .
 
 # Show the list of available images for this repo
 echo "Image is ready"
